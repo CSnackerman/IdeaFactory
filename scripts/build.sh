2bfab7cdfb7a7b4957602ed436c1833552e7b3e8
Colby <colbyackerman@gmail.com>
2020-06-05 07:22:35 -0500

    build.sh prints commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,8 +13,13 @@ executable=set
 for f in ${src_dir}/*.cc ; do
     noext=${f%.cc}
     fname=${noext##*/}
+    echo g++ -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
     g++ -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
 done
 
 # Build executable
-g++ -o ${build_dir}/${executable} ${obj_dir}/*.o -I${inc_dir} `sdl2-config --cflags --libs`
+echo g++ -o ${build_dir}/${executable} ${obj_dir}/*.o \
+    -I${inc_dir} `sdl2-config --cflags --libs`
+
+g++ -o ${build_dir}/${executable} ${obj_dir}/*.o \
+    -I${inc_dir} `sdl2-config --cflags --libs`
