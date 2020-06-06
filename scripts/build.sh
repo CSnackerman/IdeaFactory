b361cac76f687ffb9ca32d71a4c22b6a2facfb28
Colby <colbyackerman@gmail.com>
2020-06-05 19:06:27 -0500

    remove some comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,9 +6,6 @@ build_dir=../build
 
 executable=set
 
-# g++ -c *.cc -I../include
-# g++ *.o -I../include `sdl2-config --cflags --libs`
-
 # Generate object file for each source file in source directory
 for f in ${src_dir}/*.cc ; do
     noext=${f%.cc}
