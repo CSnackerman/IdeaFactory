25c6162c63d3ce221c094d34f702f91e66f3d9f7
Colby <colbyackerman@gmail.com>
2020-06-15 20:10:43 -0500

    cleanup
    code formatting

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 src_dir=../src
 inc_dir=../include
 obj_dir=../obj
@@ -6,17 +7,21 @@ build_dir=../build
 
 executable=set
 
+debug=-g
+
 # Generate object file for each source file in source directory
 for f in ${src_dir}/*.cc ; do
     noext=${f%.cc}
     fname=${noext##*/}
-    echo g++ -g -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
-    g++ -g -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
+
+    echo g++ ${debug} -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
+
+    g++ ${debug} -o ${obj_dir}/${fname}.o -c $f -I${inc_dir}
 done
 
 # Build executable
-echo g++ -g -o ${build_dir}/${executable} ${obj_dir}/*.o \
-    -I${inc_dir} `sdl2-config --cflags --libs`
+echo g++ ${debug} -o ${build_dir}/${executable} ${obj_dir}/*.o \
+   -I${inc_dir} `sdl2-config --cflags --libs`
 
-g++ -g -o ${build_dir}/${executable} ${obj_dir}/*.o \
+g++ ${debug} -o ${build_dir}/${executable} ${obj_dir}/*.o \
     -I${inc_dir} `sdl2-config --cflags --libs`
