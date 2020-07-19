b6a5b8414baba04e2bd550ecdbe811be452cfa6b
Ca <ackDev17@gmail.com>
2020-07-19 01:28:25 -0500

    compile only out-of-date source files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
 # Directories
-src_dir='../src'
-obj_dir='../obj'
-build_dir='../build'
-template_dir='../src/templates'
-game_scene_dir='../src/game_scenes'
-game_object_dir='../src/game_objects'
+src_dir="../src"
+obj_dir="../obj"
+build_dir="../build"
+template_dir="../src/templates"
+game_scene_dir="../src/game_scenes"
+game_object_dir="../src/game_objects"
 
-# Name of the executable to be built
-executable='set'
+# Name of the target to be built
+target="set"
+fullTargetPath=${build_dir}/${target}
 
-# Set debug flag
-debug='-g'
-cxxflags='-std=c++14 -Wall -Wextra'
-
+# Set compiler flags
+debug="-g"
+cxxflags="${debug} -std=c++14 -Wall -Wextra"
 
 # Output text color escape sequences
 GREEN="\u001b[32m"
-BLUE='\u001b[34m'
-CYAN='\033[1;36m'
-NO_COLOR='\033[0m'
+BLUE="\u001b[34m"
+CYAN="\033[1;36m"
+RED="\u001b[31m"
+NO_COLOR="\033[0m"
+
+# Set to false if any source file is not up-to-date and compiles
+upToDate="true"
 
 # Compile src/ directory
 printf "${CYAN}generating object files from ${GREEN}src/${NO_COLOR} :\n"
@@ -28,10 +32,21 @@ for f in ${src_dir}/*.cc ; do
 
     noext=${f%.cc}
     fname=${noext##*/}
+    header="${noext}.h"
 
-    echo g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    # Check if source file and header is up-to-date
+    if [[ ! $f -nt ${obj_dir}/${fname}.o ]]; then
+        if [[ ! ${header} -nt ${obj_dir}/${fname}.o ]]; then
+            printf "'${fname}' up-to-date\n";
+            continue;
+        fi
+    fi
 
-    g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    upToDate="false"
+
+    echo g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+
+    g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
 
 done
 
@@ -41,10 +56,19 @@ for f in ${template_dir}/*.cc ; do
 
     noext=${f%.cc}
     fname=${noext##*/}
+    header="${noext}.h"
 
-    echo g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    # Check if source file and header is up-to-date
+    if [[ ! $f -nt ${obj_dir}/${fname}.o ]]; then
+        if [[ ! ${header} -nt ${obj_dir}/${fname}.o ]]; then
+            printf "'${fname}' up-to-date\n";
+            continue;
+        fi
+    fi
 
-    g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    echo g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+
+    g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
 
 done
 
@@ -54,10 +78,21 @@ for f in ${game_object_dir}/*.cc ; do
 
     noext=${f%.cc}
     fname=${noext##*/}
+    header="${noext}.h"
 
-    echo g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    # Check if source file and header is up-to-date
+    if [[ ! $f -nt ${obj_dir}/${fname}.o ]]; then
+        if [[ ! ${header} -nt ${obj_dir}/${fname}.o ]]; then
+            printf "'${fname}' up-to-date\n";
+            continue;
+        fi
+    fi
 
-    g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    upToDate="false"
+
+    echo g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+
+    g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
 
 done
 
@@ -67,17 +102,32 @@ for f in ${game_scene_dir}/*.cc ; do
 
     noext=${f%.cc}
     fname=${noext##*/}
+    header="${noext}.h"
 
-    echo g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    # Check if source file and header is up-to-date
+    if [[ ! $f -nt ${obj_dir}/${fname}.o ]]; then
+        if [[ ! ${header} -nt ${obj_dir}/${fname}.o ]]; then
+            printf "'${fname}' up-to-date\n";
+            continue;
+        fi
+    fi
 
-    g++ ${debug} ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+    upToDate="false"
+
+    echo g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
+
+    g++ ${cxxflags} -o ${obj_dir}/${fname}.o -c $f
 
 done
 
+# Build target
+printf "\n${CYAN}building target ${RED}'${target}' ${NO_COLOR}:\n"
+if [[ ${upToDate} == "true" ]]; then
+    printf "'${target}' up-to-date\n"
+    exit;
+fi
 
-# Build executable
-printf "\n${CYAN}building executable ${GREEN}'${executable}' ${NO_COLOR}:\n"
-echo g++ ${debug} ${cxxflags} -o ${build_dir}/${executable} ${obj_dir}/*.o `sdl2-config --cflags --libs`
-g++ ${debug} ${cxxflags} -o ${build_dir}/${executable} ${obj_dir}/*.o `sdl2-config --cflags --libs`
+echo g++ ${cxxflags} -o ${build_dir}/${target} ${obj_dir}/*.o `sdl2-config --cflags --libs`
+g++ ${cxxflags} -o ${build_dir}/${target} ${obj_dir}/*.o `sdl2-config --cflags --libs`
 
 echo
